31fdc3eadecc92e5ff2fab06649bdf073834f3d2
idrk1618 <126374482+idrk1618@users.noreply.github.com>
2025-01-14 06:08:36 +0300

    update*

--- Console Manager/main.cpp
+++ Console Manager/main.cpp
@@ -3,35 +3,42 @@
 class user {
 private:
 	std::string name;
-	std::string login;
+	std::string login; // Должен быть уникальным
 	std::string mail;
 	std::string status; // Active; Needs correction; Not active; Deleted
-	std::string password; // Encrypted; hash-salt, sha256-hash или аналогичные
+	std::string password; // Должен быть зашифрован; hash-salt, sha256-hash или аналогичные
 	std::string access_level; //Admin; Manager; User
 public:
-	user(std::string nam = "", std::string log, std::string mal = "", std::string stt = "Needs correction", std::string pas, std::string acl) {
+	user(std::string log, std::string pas, std::string acl, std::string nam = "", std::string mal = "", std::string stt = "Needs correction")
+	{
 		bool allcorrect = true;
 		name = nam;
 		login = log;
 		mail = mal;
-		if (!(stt == "Active" || stt == "Needs correction" || stt == "Not active" || stt == "Deleted")) {
+		if (!(stt == "Active" || stt == "Needs correction" || stt == "Not active" || stt == "Deleted")) { // Проверка на то, что статус введён корректно
 			allcorrect = false;
 		}
-		if (!(stt == "Admin" || stt == "Manager" || stt == "User")) {
+		if (!(acl == "Admin" || acl == "Manager" || acl == "User")) { // Проверка на то, что уровень доступа введён корректно
 			allcorrect = false;
 		}
-		if (name == "" || mail == "" || !allcorrect) {
+		// При незаполнении имени, имейла или провале на одной из двух предыдущих проверок, пользователь получает статус "Needs correction"
+		if (name == "" || mail == "" || !allcorrect)
+		{
 			status = "Needs correction";
 		}
-		else {
-			status = stt; // Active; Needs correction; Not active; Deleted
+		else
+		{
+			status = stt;
 		}
-		password = pas; // Encrypted; hash-salt, sha256-hash или аналогичные
-		access_level = acl; //Admin; Manager; User
+		password = pas;
+		access_level = acl;
 	}
 };
 
+
 int main() {
+
+
 
 	return 0;
 }
